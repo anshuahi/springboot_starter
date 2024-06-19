e182ea217bb839aa98100b40e609d1e23326f0d0
Anshu Ahirwar <anshuahirwar.dakshana2015@gmail.com>
2024-06-20 02:43:59 +0530

    added register user

--- src/main/java/org/anshuahi/spring_starter/Controller/AccountController.java
+++ src/main/java/org/anshuahi/spring_starter/Controller/AccountController.java
@@ -1,12 +1,20 @@
 package org.anshuahi.spring_starter.Controller;
 
 import org.anshuahi.spring_starter.models.Account;
+import org.anshuahi.spring_starter.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/register")
     public String register(Model model) {
@@ -15,4 +23,11 @@ public class AccountController {
 
         return "register";
     }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute Account account) {
+        accountService.save(account);
+        return "redirect:/";
+    }
+
 }
