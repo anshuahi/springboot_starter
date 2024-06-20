d427116011f459f4be4ddc822278060c7f7460e0
Anshu Ahirwar <anshuahirwar.dakshana2015@gmail.com>
2024-06-21 02:07:36 +0530

    added login

--- src/main/java/org/anshuahi/spring_starter/repositories/AccountRepository.java
+++ src/main/java/org/anshuahi/spring_starter/repositories/AccountRepository.java
@@ -1,10 +1,18 @@
 package org.anshuahi.spring_starter.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 import org.anshuahi.spring_starter.models.Account;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    public static Optional<Account> findOneByEmailIgnoreCase(String email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findOneByEmailIgnoreCase'");
+    }
 
 }
--- src/main/java/org/anshuahi/spring_starter/services/AccountService.java
+++ src/main/java/org/anshuahi/spring_starter/services/AccountService.java
@@ -1,17 +1,46 @@
 package org.anshuahi.spring_starter.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 import org.anshuahi.spring_starter.models.Account;
 import org.anshuahi.spring_starter.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AccountService {
+public class AccountService implements UserDetailsService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Account save(Account account) {
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Account> optionalAccount = AccountRepository.findOneByEmailIgnoreCase(email);
+        if (!optionalAccount.isPresent()) {
+            throw new UsernameNotFoundException("Account not found");
+        }
+
+        Account account = optionalAccount.get();
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("Allow"));
+        return new User(account.getEmail(), account.getPassword(), grantedAuthorities);
     }
 
     // public
